c3af5aefba0e159ac9b032aa526de5724c2e4a9e
Асланян Вачаган <isip_v.m.aslanyan@mpt.ru>
2025-10-31 15:04:27 +0300

    second lab

---
+++ VD_games/scripts/VD_games.py
@@ -0,0 +1,2 @@
+def print_game_info():
+    print(">>> Информация из модуля VD_games загружена.")
---
+++ VD_games/scripts/VD_main.py
@@ -0,0 +1,7 @@
+from .VD_games import print_game_info
+
+def main():
+    print_game_info()
+
+if __name__ == "__main__":
+    main()
